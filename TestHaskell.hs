da91ef38c54b24f75b0e8f4d625027aa7f41b50b
Miguens Joaquin <76816697+JoacoMiguens49@users.noreply.github.com>
2024-11-07 00:04:27 -0300

    Add files via upload

--- TestHaskell.hs
+++ TestHaskell.hs
@@ -1,8 +1,22 @@
 import Test.HUnit
 import Solucion
+import Data.List
 
-run = runTestTT testGeneral
-testGeneral = validezVuelos
+-- run = runTestTT testGeneral
+-- testGeneral = validezCiudadesConectadas
+
+allTests :: Test
+allTests = test [
+    validezVuelos,
+    validezCiudadesConectadas,
+    validezModernizarFlota,
+    validezCiudadMasConectada,
+    validezSePuedeLlegar,
+    validezDuracionDelCaminoMasRapido,
+    validezPuedoVolverAOrigen
+ ]
+run = runTestTT allTests
+
 
 -- EJERCICIO 1: vuelosValidos ---------------------
 
@@ -17,12 +31,23 @@ validezVuelos = test [
     "camino invertido" ~: vuelosValidos [("cordoba", "mendoza", 5), ("mendoza", "cordoba", 5), ("chubut", "mendoza", 3)] ~?= True
  ]
 
+expectPermutacion :: (Ord a, Show a) => [a] -> [a] -> Test
+expectPermutacion actual expected = esPermutacion actual expected ~? ("expected list: " ++ show expected ++ "\nbut got: " ++ show actual)
+
+esPermutacion :: Ord a => [a] -> [a] -> Bool
+esPermutacion a b = (length a == length b) && (sort a == sort b)
+
 -- EJERCICIO 2: ciudadesConectadas-------------------
 validezCiudadesConectadas = test [
-    "todas las opciones disponibles" ~: ciudadesConectadas [("salta", "mendoza", 5), ("cordoba", "mendoza", 6), ("salta", "cordoba", 6), ("chubut", "cordoba", 3)] "cordoba" ~?= ["mendoza", "salta", "chubut"],
--- como requiere vuelosValidos (no hay repes -> (bs, ros), (bs as, ros)) pero dice que no haya repetidos, supongo que admite (bs, ros), (ros, bs as)
-    "sin repetidos" ~: ciudadesConectadas [("salta", "mendoza", 5), ("cordoba", "mendoza", 6), ("salta", "cordoba", 6), ("chubut", "cordoba", 3), ("mendoza", "cordoba", 6)] "cordoba" ~?= ["salta", "chubut", "mendoza"]
+    "todas las opciones disponibles" ~: expectPermutacion (ciudadesConectadas [("salta", "mendoza", 5), ("cordoba", "mendoza", 6), ("salta", "cordoba", 6), ("chubut", "cordoba", 3)] "cordoba") ["salta", "chubut", "mendoza"],
+    "sin repetidos" ~: expectPermutacion (ciudadesConectadas [("salta", "mendoza", 5), ("cordoba", "mendoza", 6), ("salta", "cordoba", 6), ("chubut", "cordoba", 3), ("mendoza", "cordoba", 6)] "cordoba") ["mendoza","salta", "chubut"],
+    "única conexión" ~: expectPermutacion (ciudadesConectadas [("salta", "mendoza", 5), ("formosa", "mendoza", 6), ("salta", "cordoba", 6), ("chubut", "mendoza", 3)] "cordoba") ["salta"],
+    "único vuelo" ~: expectPermutacion (ciudadesConectadas [("salta", "cordoba", 6)] "cordoba") ["salta"],
+    "varias conexiones" ~: expectPermutacion (ciudadesConectadas [("rio negro", "cordoba", 6), ("formosa", "cordoba", 6), ("cordoba", "catamarca", 6)] "cordoba") ["rio negro", "formosa", "catamarca"],
+    "agencia vacia" ~: ciudadesConectadas [] "cordoba" ~?= [],
+    "ciudad no existente" ~: ciudadesConectadas [("salta", "mendoza", 5), ("cordoba", "mendoza", 6), ("salta", "cordoba", 6), ("chubut", "cordoba", 3), ("catamarca", "cordoba", 6)] "formosa" ~?= []
  ]
+
 
 
 -- EJERCICIO 3: modernizarFlota ---------------------
@@ -32,10 +57,18 @@ validezModernizarFlota = test [
     "mismo origen y destino"~: modernizarFlota [("neuquen", "cordoba", 16), ("salta", "formosa", 50)] ~?= [("neuquen", "cordoba", 14.4), ("salta", "formosa", 45.0)]
  ]
 
+
 -- EJERCICIO 4: ciudadMasConectada-------------------
+expectAny :: (Foldable t, Eq a, Show a, Show (t a)) => a -> t a -> Test
+expectAny actual expected = elem actual expected ~? ("expected any of: " ++ show expected ++ "\n but got: " ++ show actual)
+
 validezCiudadMasConectada = test [
     "mayor conexion es correcta" ~: ciudadMasConectada [("salta", "cordoba", 5), ("chubut", "mendoza", 3), ("formosa", "cordoba", 9)] ~?= "cordoba",
-    "Varias Ciudade con mismas apariciones" ~: ciudadMasConectada [("salta", "cordoba", 5), ("chubut", "mendoza", 3), ("formosa", "chubut", 9)] ~?= "chubut"
+    {-nuevo-}
+    "unico vuelo" ~: expectAny (ciudadMasConectada [("salta", "cordoba", 5)]) ["cordoba", "salta"],
+    "empate vuelos entre 3" ~: expectAny (ciudadMasConectada [("salta", "cordoba", 5), ("chubut", "salta", 3), ("cordoba", "chubut", 9)]) ["salta", "cordoba", "chubut"],
+    "empate vuelos entre 2" ~: expectAny (ciudadMasConectada [("salta", "cordoba", 5), ("chubut", "salta", 3), ("cordoba", "chubut", 9), ("salta", "mendoza", 7), ("chubut", "mendoza", 5)]) ["chubut", "salta"],
+    "funcionamiento correcto" ~: ciudadMasConectada [("salta", "cordoba", 5), ("chubut", "salta", 3), ("cordoba", "chubut", 9), ("salta", "mendoza", 7)] ~?= "salta"
  ]
 
 
@@ -44,7 +77,8 @@ validezSePuedeLlegar = test [
     "vuelo directo" ~: sePuedeLlegar [("salta", "mendoza", 5), ("cordoba", "mendoza", 6), ("salta", "cordoba", 6), ("chubut", "cordoba", 3)] "salta" "cordoba" ~?= True,
     "vuelo con escala" ~: sePuedeLlegar [("salta", "mendoza", 5), ("formosa", "mendoza", 6), ("chubut", "cordoba", 3), ("mendoza", "cordoba", 6)] "salta" "cordoba" ~?= True,
     "vuelo directo y con escala"~: sePuedeLlegar [("salta", "mendoza", 5), ("formosa", "mendoza", 6), ("salta", "cordoba", 6), ("chubut", "cordoba", 3), ("mendoza", "cordoba", 6)] "salta" "cordoba" ~?= True,
-    "sin vuelos" ~: sePuedeLlegar [("salta", "mendoza", 5), ("cordoba", "mendoza", 6), ("salta", "formosa", 6), ("chubut", "cordoba", 3)] "salta" "cordoba" ~?= False
+    "sin vuelos" ~: sePuedeLlegar [("salta", "mendoza", 5), ("cordoba", "mendoza", 6), ("salta", "formosa", 6), ("chubut", "cordoba", 3)] "salta" "cordoba" ~?= False,
+    "agencia vacia" ~: sePuedeLlegar [] "salta" "cordoba" ~?= False
  ]
 
 
